c84deec4425ae2adaa1dcd2713bda75940221ed8
João Maroeli <maroelijoao@gmail.com>
2021-03-28 16:50:22 -0300

    Started challenge

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,13 @@
+import Link from 'next/link';
+
+import styles from './header.module.scss';
+
 export default function Header() {
-  // TODO
+  return (
+    <Link href="/">
+      <header className={styles.header}>
+        <img src="/spacetraveling.svg" alt="logo" />
+      </header>
+    </Link>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,8 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+
+import Header from '../components/Header';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +28,38 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | spacetraveling</title>
+      </Head>
+
+      <main className={styles.container}>
+        <Header />
+
+        <section className={styles.content}>
+          <div>
+            <h1>Como utilizar Hooks</h1>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            
+            <div>
+              <span>
+                <FiCalendar />
+                15 Mar 2021
+              </span>
+
+              <span>
+                <FiUser />
+                16 Mar 2021
+              </span>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
